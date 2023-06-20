632fcf740d27298aa96fd87b8eff02e59a92acc3
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-20 05:38:55 -0700

    Update city.py

--- models/city.py
+++ models/city.py
@@ -13,6 +13,6 @@ class City(BaseModel, Base):
     """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-     places = relationship("Place", cascade='all, delete, delete-orphan',
+    places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="cities")
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
