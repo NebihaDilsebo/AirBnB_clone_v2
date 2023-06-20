85220d12cbbc6aeabf2b682073df4ba3b1d8b803
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-20 04:03:14 -0700

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""
+This module defines the User class, which inherits from BaseModel and Base.
+It represents a user in the AirBnB clone project.
+"""
+
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines the User model for the database table 'users'.
+    attributes:
+            email = 'email address'
+            password = 'password for you login'
+            first_name = 'first name'
+            last_name = 'last name'
+    """
+
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new User instance."""
+        super().__init__(*args, **kwargs)
