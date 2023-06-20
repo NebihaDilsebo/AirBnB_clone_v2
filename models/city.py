70c4c2a765872ac806b909906b4487eccb378db2
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-20 04:41:53 -0700

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ This class defines the City model for the database table 'cities'.
+    """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+     places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
