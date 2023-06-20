1f213190718c706fc29c28bf2dc65905249cf9fb
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-20 05:09:23 -0700

    Update place.py

--- models/place.py
+++ models/place.py
@@ -19,52 +19,52 @@ place_amenity = Table("place_amenity", Base.metadata,
 
 
 class Place(BaseModel, Base):
-  """ This class defines the Place model for the database table 'places'.
-  """
-  __tablename__ = 'places'
-  city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-  user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-  name = Column(String(128), nullable=False)
-  description = Column(String(1024), nullable=True)
-  number_rooms = Column(Integer, nullable=False, default=0)
-  number_bathrooms = Column(Integer, nullable=False, default=0)
-  max_guest = Column(Integer, nullable=False, default=0)
-  price_by_night = Column(Integer, nullable=False, default=0)
-  latitude = Column(Float, nullable=True)
-  longitude = Column(Float, nullable=True)
-  amenity_ids = []
+    """ This class defines the Place model for the database table 'places'.
+    """
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
-  if getenv("HBNB_TYPE_STORAGE") == "db":
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
 
-    amenities = relationship("Amenity", secondary=place_amenity,
+        amenities = relationship("Amenity", secondary=place_amenity,
                                  viewonly=False,
                                  back_populates="place_amenities")
-  else:
-    @property
-    def reviews(self):
-      """ Returns list of reviews.id """
-      var = models.storage.all()
-      lista = []
-      result = []
-      for key in var:
-        review = key.replace('.', ' ')
-        review = shlex.split(review)
-        if (review[0] == 'Review'):
-          lista.append(var[key])
-      for elem in lista:
-        if (elem.place_id == self.id):
-          result.append(elem)
-      return (result)
+      else:
+        @property
+        def reviews(self):
+            """ Returns list of reviews.id """
+            var = models.storage.all()
+            listb = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    listb.append(var[key])
+            for elem in lista:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
 
-    @property
-    def amenities(self):
-      """ Returns list of amenity ids """
-      return self.amenity_ids
+        @property
+        def amenities(self):
+            """ Returns list of amenity ids """
+            return self.amenity_ids
 
-    @amenities.setter
-    def amenities(self, obj=None):
-      """ Appends amenity ids to the attribute """
-      if type(obj) is Amenity and obj.id not in self.amenity_ids:
-        self.amenity_ids.append(obj.id)
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribute """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
