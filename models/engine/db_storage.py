9449cf00812ce78f18c326d6dc635c3861799a0a
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-21 11:45:20 -0700

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" new class for sqlAlchemy """
+""" This module defines a class to manage database storage for hbnb clone.
+"""
 from os import getenv
-from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
 from models.base_model import Base
 from models.state import State
 from models.city import City
@@ -14,17 +16,23 @@ from models.amenity import Amenity
 
 
 class DBStorage:
-    """ create tables in environmental"""
+    """ This class manages storage of hbnb models in a SQL database.
+    Private class attributes:
+        __engine: the engine
+        __session: the session
+    """
     __engine = None
     __session = None
 
     def __init__(self):
+        """Initializes the SQL database storage."""
         user = getenv("HBNB_MYSQL_USER")
         passwd = getenv("HBNB_MYSQL_PWD")
         db = getenv("HBNB_MYSQL_DB")
         host = getenv("HBNB_MYSQL_HOST")
         env = getenv("HBNB_ENV")
 
+        """ to create the engine."""
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
                                       .format(user, passwd, host, db),
                                       pool_pre_ping=True)
@@ -33,9 +41,7 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
+        """Returns a dictionary of models currently in storage
         """
         dic = {}
         if cls:
@@ -55,30 +61,29 @@ class DBStorage:
         return (dic)
 
     def new(self, obj):
-        """add a new element in the table
-        """
+        """add the object to the current database session."""
         self.__session.add(obj)
 
     def save(self):
-        """save changes
-        """
+        """commit all changes of the current database session."""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete an element in the table
-        """
+        """delete from the current database session."""
         if obj:
             self.session.delete(obj)
 
     def reload(self):
-        """configuration
+        """
+        create all tables in the database (feature of SQLAlchemy)
+        (WARNING: all classes who inherit from Base must be imported
+        before calling Base.metadata.create_all(engine))
         """
         Base.metadata.create_all(self.__engine)
-        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sec)
+        sess = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess)
         self.__session = Session()
 
     def close(self):
-        """ calls remove()
-        """
+        """ calls remove()."""
         self.__session.close()
