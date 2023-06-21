e299d955b4d9e59f77593c7244ac2c2195ae8d25
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-21 04:42:19 -0700

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
 place_amenity = Table("place_amenity",Base.metadata,
-        Column("place_id", string(60),
+        Column("place_id", String(60),
             ForeignKey("places.id"),
             primary_key=True, nullable=False),
         Column("amenity_id", String(60),
