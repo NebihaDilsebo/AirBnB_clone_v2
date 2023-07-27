b3cd509e372c16614ed8624e38a9fda21bcd32dc
NebihaDilsebo <shegolenur@hotmail.com>
2023-07-27 02:48:53 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
-"""This module defines the State class."""
-import os
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-
 from models.city import City
-import shlex
+import os
 
 
 class State(BaseModel, Base):
-    """This class defines the State model for the database table 'states'.
-    State inherits from BaseModel and Base (respect the order).
-    """
+    """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
 
     @property
     def cities(self):
-        """Returns the cities in this State."""
-        val = models.storage.all()
-        lista = []
-        result = []
-        for key in val:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(val[key])
-        for elemt in lista:
-            if (elemt.state_id == self.id):
-                result.append(elemt)
-        return (result)
+        """getter attribute cities that returns the list of City"""
+        from models import storage
+        my_list = []
+        extracted_cities = storage.all(City).values()
+        for city in extracted_cities:
+            if self.id == city.state_id:
+                my_list.append(city)
+        return my_list
