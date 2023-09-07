8a6a66f305a0a4c18b4bf4b5deb5ae7546e58d51
NebihaDilsebo <shegolenur@hotmail.com>
2023-09-07 08:42:03 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,68 +1,56 @@
 #!/usr/bin/python3
-'''
-fabric script to distribute an archive to web servers
-----NEEDS TO REVISIT SCRIPT
-'''
-
+"""
+Fabric script to distribute an archive to web servers.
+"""
+from fabric.api import *
 import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
 
+# Define the username and SSH key file
+env.user = 'ubuntu'
+env.key_filename = '/path/to/your/ssh/private/key'
 
+# List of web server IP addresses
 env.hosts = ['107.21.37.236', '54.89.179.235']
 
-
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
-
-
-def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
+    Distribute an archive to web servers and deploy it.
     """
     if not os.path.exists(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
+
+    # Extract the filename from the archive path
+    archive_filename = os.path.basename(archive_path)
+    archive_no_ext = os.path.splitext(archive_filename)[0]
+
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
-    except Exception:
-        success = False
-    return success
+        # Upload the archive to /tmp/ directory on the web server
+        put(archive_path, '/tmp/')
+
+        # Create the target directory for deployment
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_no_ext))
+
+        # Extract the archive to the target directory
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(archive_filename, archive_no_ext))
+
+        # Remove the uploaded archive
+        run('rm /tmp/{}'.format(archive_filename))
+
+        # Move the contents from the extracted folder to the target directory
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}'.format(archive_no_ext, archive_no_ext))
+
+        # Remove the empty folder
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_no_ext))
+
+        # Remove the current symlink if it exists
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symlink to the deployed version
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'.format(archive_no_ext))
+
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        return False
+
